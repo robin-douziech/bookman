f9e791e9db827b41a44ee37c30a6424360cdc526
Naval Cristian <telumletiferum@protonmail.com>
2023-12-09 16:12:54 +0100

    Revert "Using bootstrap CSS on the user creation"
    This reverts commit 4e6697c39727773b145ed75166a39d8518f71b5b.

--- bookman_project/user/forms.py
+++ bookman_project/user/forms.py
@@ -4,69 +4,64 @@ from django.utils.translation import gettext_lazy as _
 
 from . import models
 
+class LoginForm(forms.Form) :
 
-class LoginForm(forms.Form):
+	username = forms.CharField(
+		label="Username",
+		max_length=50
+	)
 
-    username = forms.CharField(
-        label="Username",
-        max_length=50
-    )
+	password = forms.CharField(
+		label="Password",
+		max_length=50,
+		widget=forms.PasswordInput()
+	)
 
-    password = forms.CharField(
-        label="Password",
-        max_length=50,
-        widget=forms.PasswordInput()
-    )
+class UserCreationForm(forms.Form) :
 
+	username = forms.CharField(
+		label="Username",
+		max_length=50
+	)
 
-class UserCreationForm(forms.Form):
-    username = forms.CharField(
-        label="Username",
-        max_length=50,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
-    password1 = forms.CharField(
-        label="Password",
-        max_length=50,
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    password2 = forms.CharField(
-        label="Confirm password",
-        max_length=50,
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
-    )
-    email = forms.EmailField(
-        label="Email",
-        widget=forms.EmailInput(attrs={'class': 'form-control'})
-    )
+	password1 = forms.CharField(
+		label="Password",
+		max_length=50,
+		widget=forms.PasswordInput()
+	)
 
-    def clean(self):
-        username = self.cleaned_data['username']
-        password1 = self.cleaned_data['password1']
-        password2 = self.cleaned_data['password2']
-        email = self.cleaned_data['email']
+	password2 = forms.CharField(
+		label="Confirm password",
+		max_length=50,
+		widget=forms.PasswordInput()
+	)
 
-        for user in models.User.objects.all():
-            if user.username == username:
-                raise ValidationError(_("Username %(username)s already used."), params={
-                                      "username": username})
-            elif user.email == email:
-                raise ValidationError(
-                    _("E-mail %(email)s already used."), params={"email": email})
-        if password1 != password2:
-            raise ValidationError(_("Entered password differ."))
+	email = forms.EmailField(label="Email")
 
-        return self.cleaned_data
+	def clean(self) :
+		username = self.cleaned_data['username']
+		password1 = self.cleaned_data['password1']
+		password2 = self.cleaned_data['password2']
+		email = self.cleaned_data['email']
 
+		for user in models.User.objects.all() :
+			if user.username == username :
+				raise ValidationError(_("Username %(username)s already used."), params={"username": username})
+			elif user.email == email :
+				raise ValidationError(_("E-mail %(email)s already used."), params={"email": email})
+		if password1 != password2 :
+			raise ValidationError(_("Entered password differ."))
 
-class UserSearchForm(forms.Form):
+		return self.cleaned_data
 
-    search_txt = forms.CharField(
-        label="Search text",
-        max_length=50,
-        required=False
-    )
+class UserSearchForm(forms.Form) :
 
-    def __init__(self, request, *args, **kwargs):
-        super(UserSearchForm, self).__init__(*args, **kwargs)
-        self.fields['search_txt'].initial = request.GET.get('search', '')
+	search_txt = forms.CharField(
+		label = "Search text",
+		max_length = 50,
+		required = False
+	)
+
+	def __init__(self, request, *args, **kwargs) :
+		super(UserSearchForm, self).__init__(*args, **kwargs)
+		self.fields['search_txt'].initial = request.GET.get('search', '')
