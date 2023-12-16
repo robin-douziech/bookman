be51a65956690f31264e949a24f75d9f1b08fb16
Naval Cristian <telumletiferum@protonmail.com>
2023-12-16 22:28:15 +0100

    Reformatted python files with autopep8

--- bookman_project/book/admin.py
+++ bookman_project/book/admin.py
@@ -2,18 +2,22 @@ from django.contrib import admin
 
 from . import models
 
+
 @admin.register(models.Author)
-class AuthorAdmin(admin.ModelAdmin) :
-	pass
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(models.Publisher)
-class PublisherAdmin(admin.ModelAdmin) :
-	pass
+class PublisherAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(models.Genre)
-class GenreAdmin(admin.ModelAdmin) :
-	pass
+class GenreAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(models.Book)
-class BookAdmin(admin.ModelAdmin) :
-	pass
+class BookAdmin(admin.ModelAdmin):
+    pass
--- bookman_project/book/urls.py
+++ bookman_project/book/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 app_name = "book"
 urlpatterns = [
-	path("", views.index, name="index"),
-	path("details", views.details, name="details"),
+    path("", views.index, name="index"),
+    path("details", views.details, name="details"),
 
-	path("new-author", views.author_creation, name="author_creation"),
-	path("new-publisher", views.publisher_creation, name="publisher_creation"),
-	path("new-genre", views.genre_creation, name="genre_creation"),
-	path("new-book", views.book_creation, name="book_creation")
+    path("new-author", views.author_creation, name="author_creation"),
+    path("new-publisher", views.publisher_creation, name="publisher_creation"),
+    path("new-genre", views.genre_creation, name="genre_creation"),
+    path("new-book", views.book_creation, name="book_creation")
 ]
